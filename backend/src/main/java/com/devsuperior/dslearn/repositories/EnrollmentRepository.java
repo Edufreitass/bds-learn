3ece02e9c659247ce83950b013d756a3897c896e
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-01-11 10:44:57 -0300

    Fix: EnrollmentRepository

--- backend/src/main/java/com/devsuperior/dslearn/repositories/EnrollmentRepository.java
+++ backend/src/main/java/com/devsuperior/dslearn/repositories/EnrollmentRepository.java
@@ -3,7 +3,8 @@ package com.devsuperior.dslearn.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dslearn.entities.Enrollment;
+import com.devsuperior.dslearn.entities.pk.EnrollmentPK;
 
-public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+public interface EnrollmentRepository extends JpaRepository<Enrollment, EnrollmentPK> {
 
 }
